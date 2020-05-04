337176da1368f8f069d050159525bd334f78ca51
Aishwarya Barai <ashhbarai008@gmail.com>
2020-05-04 09:58:24 -0400

    Add files via upload

---
+++ dict_simple.py
@@ -0,0 +1,9 @@
+num = {
+    '1':'One', '2':'Two', '3': 'Three', '4': 'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eight', '9':'Nine', '0':'Zero'
+}
+ph_num = input('enter your phone number: ')
+output = ''
+for ch in ph_num:
+    output+= num.get(ch) + ' '
+print(output)
+
